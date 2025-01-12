728f037167458c7c15425ad0801047cd1a6dcb29
Halyne Diogo <halyneteen@gmail.com>
2025-01-12 19:27:20 -0300

    Removed repository inside ignite_Gym and added it as a folder

---
+++ ignite_Gym/App.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto'
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {fontsLoaded ? <Text>Home</Text> : <View />}
+      </View>
+
+  );
+}
+
